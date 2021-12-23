d2400c656f52a0f641ec0fcd8dee0da25028abb2
Matthew Blank <m.p.pusty@gmail.com>
2021-12-23 17:19:44 +0100

    Project cleanup

--- Project/BouncingBall/Assets/_Game/Scripts/UI/RestartButtonController.cs
+++ Project/BouncingBall/Assets/_Game/Scripts/UI/RestartButtonController.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RestartButtonController : MonoBehaviour
 {
+    public BallController ball;
+    
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
